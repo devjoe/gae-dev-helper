c8fdf5283fbd3349e28d395e14668ec1fb8b14b7
Joe <excusemejoe@gmail.com>
2016-11-17 00:48:45 +0800

    remove sample code (from click website)

--- gae.py
+++ gae.py
@@ -12,15 +12,6 @@ def gae():
     pass
 
 
-@gae.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
-
 
 @gae.command()
 @click.option('-c', '--code', 'code', nargs=1, type=click.STRING)
