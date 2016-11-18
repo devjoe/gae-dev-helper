fe1d75065f7371502cf81ea57e2a1019c2db093c
Joe <excusemejoe@gmail.com>
2016-11-18 15:33:20 +0800

    add config.py

---
+++ custom_config.py
@@ -0,0 +1,21 @@
+# ===== GAE dev_appserver.py settings =====
+# [Required]
+gae_sdk_path = ""
+project_path = ""
+
+
+# [Optional]
+datastore_path = ""
+port = ""
+admin_port = ""
+
+
+# ===== GAE Helper settings =====
+# [Log]
+log_path = ""
+append_date_to_log = False
+
+# [Request Filter]
+file_type_filter = []
+custom_regex_filter = []
+use_time_delimiter = False
